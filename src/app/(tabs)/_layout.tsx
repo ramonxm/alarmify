2339714c5d8be48b7a7a389f4dd8155d33458b1c
Ramon Xavier <ramon1234rxm@gmail.com>
2024-09-16 20:57:39 -0300

    feat: create bottom tabs navigation

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+import { Alarm } from "@/assets/svgs/Alarm";
+import { Stopwatch } from "@/assets/svgs/Stopwatch";
+import { Timer } from "@/assets/svgs/Timer";
+import { WorldClock } from "@/assets/svgs/WorldClock";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: "#000" },
+        tabBarActiveTintColor: "#F4B657",
+        tabBarInactiveTintColor: "#9B9B9B",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "World Clock",
+          tabBarIcon: ({ color }) => <WorldClock color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="alarm"
+        options={{
+          title: "Alarm",
+          tabBarIcon: ({ color }) => <Alarm color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="stopwatch"
+        options={{
+          title: "Stopwatch",
+          tabBarIcon: ({ color }) => <Stopwatch color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="timer"
+        options={{
+          title: "Timer",
+          tabBarIcon: ({ color }) => <Timer color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import 'react-native-reanimated';
-import { useColorScheme } from '../hooks/useColorScheme';
-import { useEffect } from 'react';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
+import { useColorScheme } from "../hooks/useColorScheme";
+import { useEffect } from "react";
 
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   useEffect(() => {
-    SplashScreen.hideAsync()
-  }, [])
+    SplashScreen.hideAsync();
+  }, []);
 
-   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
