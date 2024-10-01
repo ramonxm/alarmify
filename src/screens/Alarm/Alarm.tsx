731f4d7c32e3c08ef35a12569ebfc27fe888217d
Ramon Xavier <ramon1234rxm@gmail.com>
2024-10-01 20:11:33 -0300

    chore: making test with restyle

--- src/screens/Alarm/Alarm.tsx
+++ src/screens/Alarm/Alarm.tsx
@@ -1,21 +1,24 @@
 import { ActionBar } from "@/components/ActionBar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { createBox, createText } from "@shopify/restyle";
+
+const Box = createBox();
+const Text = createText();
 
 export const AlarmScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <ActionBar />
-      <View style={{ marginLeft: 16, marginTop: 16 }}>
-        <Text style={{ color: "#fff" }}>Alarm</Text>
-        <Text style={{ color: "#fff" }}>Sleep | Wake Up</Text>
-      </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Box flex={1} backgroundColor="background">
+        <ActionBar />
+        <Box marginLeft="s" marginTop="s">
+          <Text variant="body" color="text">
+            Alarm
+          </Text>
+          <Text variant="body" color="text">
+            Sleep | Wake Up
+          </Text>
+        </Box>
+      </Box>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-  },
-});
